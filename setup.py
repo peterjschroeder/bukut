32465a694103c61e02ad83534ba68a319b9230fa
peter <peterjschroeder@gmail.com>
2022-01-07 09:02:18 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+        name='bukut', 
+        version='0.1', 
+        description='Bookmarks manager', 
+        author='Peter J. Schroeder', 
+        author_email='peterjschroeder@gmail.com', 
+        url='https://github.com/peterjschroeder/bukut',
+        scripts=['bukut'],
+        install_requires=['asciimatics', 'beautifulsoup4', 'natsort', 'pyperclip']
+)
+
